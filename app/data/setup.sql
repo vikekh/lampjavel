f87c13a104fa1f4684eecb4a3030783ff8b3e2fb
vikekh <vieekk@gmail.com>
2015-01-18 18:53:44 +0100

    New table: lampjavel_users

--- app/data/setup.sql
+++ app/data/setup.sql
@@ -13,6 +13,15 @@ create table `lampjavel_channels` (
     primary key (`id`)
 );
 
+create table `lampjavel_users` (
+    `id` varchar(20) not null,
+    `email` varchar(100) not null,
+    `password_hash` varchar(255),
+    `created_timestamp` timestamp not null default '0000-00-00 00:00:00',
+    `updated_timestamp` timestamp not null default current_timestamp on update current_timestamp,
+    primary key (`id`)
+)
+
 create table `lampjavel_channel_images` (
     `channel_id` varchar(20) not null,
     `image_id` int(9) not null,
