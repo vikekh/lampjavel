33ddf3fd45cf1863a36825f2a00b08801cd3d1bd
vikekh <vikekhse@gmail.com>
2015-01-19 22:41:22 +0100

    A first take on the database design

--- app/data/setup.sql
+++ app/data/setup.sql
@@ -1,16 +1,25 @@
+drop table if exists `lampjavel_category_images`;
+drop table if exists `lampjavel_channel_images`;
+drop table if exists `lampjavel_channel_viewers`;
+drop table if exists `lampjavel_channel_admins`;
+drop table if exists `lampjavel_users`;
+drop table if exists `lampjavel_categories`;
+drop table if exists `lampjavel_channels`;
+drop table if exists `lampjavel_images`;
+
 create table `lampjavel_images` (
     `id` int(9) not null auto_increment,
-    `image_url` varchar(100) not null,
-    `created_timestamp` timestamp not null default '0000-00-00 00:00:00',
-    `updated_timestamp` timestamp not null default current_timestamp on update current_timestamp,
+    `url` varchar(100) not null,
+    `created` timestamp not null default '0000-00-00 00:00:00',
+    `updated` timestamp not null default current_timestamp on update current_timestamp,
     primary key (`id`)
 );
 
 create table `lampjavel_channels` (
     `name` varchar(20) not null,
     `public` boolean,
-    `created_timestamp` timestamp not null default '0000-00-00 00:00:00',
-    `updated_timestamp` timestamp not null default current_timestamp on update current_timestamp,
+    `created` timestamp not null default '0000-00-00 00:00:00',
+    `updated` timestamp not null default current_timestamp on update current_timestamp,
     primary key (`name`)
 );
 
@@ -18,8 +27,8 @@ create table `lampjavel_categories` (
     `name` varchar(20) not null,
     `channel_name` varchar(20) default null,
     `public` boolean,
-    `created_timestamp` timestamp not null default '0000-00-00 00:00:00',
-    `updated_timestamp` timestamp not null default current_timestamp on update current_timestamp,
+    `created` timestamp not null default '0000-00-00 00:00:00',
+    `updated` timestamp not null default current_timestamp on update current_timestamp,
     primary key (`name`),
     foreign key (`channel_name`) references `lampjavel_channels` (`name`)
 );
@@ -27,16 +36,43 @@ create table `lampjavel_categories` (
 create table `lampjavel_users` (
     `username` varchar(20) not null,
     `email` varchar(100) not null,
-    `password_hash` varchar(255),
-    `created_timestamp` timestamp not null default '0000-00-00 00:00:00',
-    `updated_timestamp` timestamp not null default current_timestamp on update current_timestamp,
+    `password_hash` varchar(255) default null,
+    `created` timestamp not null default '0000-00-00 00:00:00',
+    `updated` timestamp not null default current_timestamp on update current_timestamp,
     primary key (`username`)
+);
+
+create table `lampjavel_channel_admins` (
+	`channel_name` varchar(20) default null,
+	`username` varchar(20) not null,
+    foreign key (`channel_name`) references `lampjavel_channels` (`name`),
+    foreign key (`username`) references `lampjavel_users` (`username`),
+    unique (`channel_name`)
+);
+
+create table `lampjavel_channel_viewers` (
+	`channel_name` varchar(20) default null,
+	`username` varchar(20) not null,
+    foreign key (`channel_name`) references `lampjavel_channels` (`name`),
+    foreign key (`username`) references `lampjavel_users` (`username`),
+    unique (`channel_name`, `username`)
 );
 
 create table `lampjavel_channel_images` (
     `channel_name` varchar(20) not null,
     `image_id` int(9) not null,
-    unique (`channel_id`, `image_id`),
     foreign key (`channel_name`) references `lampjavel_channels` (`name`),
-    foreign key (`image_id`) references `lampjavel_images` (`id`)
+    foreign key (`image_id`) references `lampjavel_images` (`id`),
+    unique (`channel_name`, `image_id`)
 );
+
+create table `lampjavel_category_images` (
+    `category_name` varchar(20) not null,
+    `image_id` int(9) not null,
+    foreign key (`category_name`) references `lampjavel_categories` (`name`),
+    foreign key (`image_id`) references `lampjavel_images` (`id`),
+    unique (`category_name`, `image_id`)
+);
+
+insert into `lampjavel_users` (`username`, `email`, `created`, `updated`)
+values ('admin', 'admin@admin.com', null, null);
