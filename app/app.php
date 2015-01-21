a9be672b9370e83822d07b65ba9d107092e6b4cf
vikekh <vikekhse@gmail.com>
2015-01-22 00:44:24 +0100

    REST to database test

--- app/app.php
+++ app/app.php
@@ -1,11 +1,32 @@
 <?php
 
 require '../vendor/autoload.php';
+require '../app/config.php';
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+
+$capsule = new Capsule;
+$capsule->addConnection($config);
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
 
 $app = new \Slim\Slim();
 
 $app->get('/', function () use ($app) {
-    echo 'test';
+    echo '?';
+});
+
+$app->get('/images', function () use ($app) {
+    $images = \Image::all();
+    echo $images->toJson();
+});
+
+$app->get('/images/:channelName', function ($channelName) use ($app) {
+    $images = \Image::where('channel_name', '=', $channelName)->get();
+    echo $images->toJson();
 });
 
 $app->run();
---
+++ app/models/Image.php
@@ -0,0 +1,5 @@
+<?php
+
+class Image extends \Illuminate\Database\Eloquent\Model {
+	protected $table = 'lampjavel_images';
+}
