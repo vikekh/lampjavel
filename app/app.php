5c31cc418fb8d4578ab8ffcaa56fbf3d4a3da0ff
vikekh <vikekhse@gmail.com>
2015-02-04 21:23:35 +0100

    More Apache tinkering and rename again

--- app/app.php
+++ app/app.php
@@ -1,7 +1,7 @@
 <?php
 
 require '../vendor/autoload.php';
-require '../api/config.php';
+require '../app/config.php';
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Illuminate\Events\Dispatcher;
