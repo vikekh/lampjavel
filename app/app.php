4148553c3e259ce1f5980624e15d2f66cf403a65
vikekh <vikekhse@gmail.com>
2015-02-08 23:40:49 +0100

    Removed grouping for now

--- app/app.php
+++ app/app.php
@@ -14,65 +14,61 @@ $app->get('/', function () use ($app) {
     echo '/';
 });
 
-$app->group('/channels', function () use ($app) {
+$app->post('/channels', function () use ($app) {
+    $channel = new \Channel;
 
-    $app->post('/', function () use ($app) {
-        $channel = new \Channel;
+    if ($name = $app->request->post('name')) {
+        $channel->name = $name;
+    }
 
-        if ($name = $app->request->post('name')) {
-            $channel->name = $name;
+    if ($public = $app->request->post('public')) {
+        $channel->public = boolval($public);
+    }
+
+    $channel->created = null;
+    $channel->updated = null;
+    $channel->save();
+
+    echo $channel->toJson();
+});
+
+$app->post('/channels/:channelName/images', function ($channelName) use ($app) {
+    $image = new \Image;
+
+    if ($url = $app->request->post('url')) {
+        $image->url = $url;
+    }
+
+    $image->created = null;
+    $image->updated = null;
+    $image->save();
+    $image->channels()->sync(array($channelName));
+
+    echo $image->toJson();
+});
+
+$app->get('/channels/:channelName/images(/:imageId)', function ($channelName, $imageId = null) use ($app) {
+    $images = \Channel::find($channelName)->images(); //->where('image_i', '=', $channelName);
+
+    if ($imageId != null) {
+        $images = $images->find(intval($imageId));
+    }
+
+    if ($orderBy = $app->request->get('orderby')) {
+        switch ($orderBy) {
+            case 'random':
+                $images = $images->orderByRaw('RAND()');
+                break;
         }
+    }
 
-        if ($public = $app->request->post('public')) {
-            $channel->public = boolval($public);
+    if ($limit = $app->request->get('limit')) {
+        if (is_numeric($limit)) {
+            $images = $images->take(intval($limit));
         }
+    }
 
-        $channel->created = null;
-        $channel->updated = null;
-        $channel->save();
-
-        echo $channel->toJson();
-    });
-
-    $app->post('/:channelName/images', function ($channelName) use ($app) {
-        $image = new \Image;
-
-        if ($url = $app->request->post('url')) {
-            $image->url = $url;
-        }
-
-        $image->created = null;
-        $image->updated = null;
-        $image->save();
-        $image->channels()->sync(array($channelName));
-
-        echo $image->toJson();
-    });
-
-    $app->get('/:channelName/images(/:imageId)', function ($channelName, $imageId = null) use ($app) {
-        $images = \Channel::find($channelName)->images(); //->where('image_i', '=', $channelName);
-
-        if ($imageId != null) {
-            $images = $images->find(intval($imageId));
-        }
-
-        if ($orderBy = $app->request->get('orderby')) {
-            switch ($orderBy) {
-                case 'random':
-                    $images = $images->orderByRaw('RAND()');
-                    break;
-            }
-        }
-
-        if ($limit = $app->request->get('limit')) {
-            if (is_numeric($limit)) {
-                $images = $images->take(intval($limit));
-            }
-        }
-
-        echo $images->get()->toJson();
-    });
-
+    echo $images->get()->toJson();
 });
 
 $app->run();
