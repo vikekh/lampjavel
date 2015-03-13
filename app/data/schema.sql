fd39f73415656cc86403be075760ab6db4a8231c
vikekh <vikekhse@gmail.com>
2015-03-14 00:46:27 +0100

    Renamed database table columns

--- app/data/schema.sql
+++ app/data/schema.sql
@@ -8,80 +8,80 @@ drop table if exists `lampjavel_images`;
 drop table if exists `lampjavel_channels`;
 
 create table `lampjavel_channels` (
-    `name` varchar(20) not null,
-    `public` boolean,
-    `created` timestamp not null default '0000-00-00 00:00:00',
-    `updated` timestamp not null default current_timestamp on update current_timestamp,
-    primary key (`name`)
+    `id` varchar(20) not null,
+    `is_public` boolean,
+    `created_at` timestamp not null default '0000-00-00 00:00:00',
+    `updated_at` timestamp not null default current_timestamp on update current_timestamp,
+    primary key (`id`)
 );
 
 create table `lampjavel_images` (
     `id` int(9) not null auto_increment,
     `url` varchar(100) not null,
-    `created` timestamp not null default '0000-00-00 00:00:00',
-    `updated` timestamp not null default current_timestamp on update current_timestamp,
+    `created_at` timestamp not null default '0000-00-00 00:00:00',
+    `updated_at` timestamp not null default current_timestamp on update current_timestamp,
     primary key (`id`)
 );
 
 create table `lampjavel_tags` (
-    `name` varchar(20) not null,
-    `created` timestamp not null default '0000-00-00 00:00:00',
-    `updated` timestamp not null default current_timestamp on update current_timestamp,
-    primary key (`name`)
+    `id` varchar(20) not null,
+    `created_at` timestamp not null default '0000-00-00 00:00:00',
+    `updated_at` timestamp not null default current_timestamp on update current_timestamp,
+    primary key (`id`)
 );
 
 create table `lampjavel_users` (
-    `username` varchar(20) not null,
+    `id` varchar(20) not null,
     `email` varchar(100) not null,
     `password_hash` varchar(255) default null,
-    `created` timestamp not null default '0000-00-00 00:00:00',
-    `updated` timestamp not null default current_timestamp on update current_timestamp,
-    primary key (`username`)
+    `created_at` timestamp not null default '0000-00-00 00:00:00',
+    `updated_at` timestamp not null default current_timestamp on update current_timestamp,
+    primary key (`id`)
 );
 
 create table `lampjavel_channel_admins` (
-    `channel_name` varchar(20) not null,
-    `username` varchar(20) not null,
-    foreign key (`channel_name`) references `lampjavel_channels` (`name`),
-    foreign key (`username`) references `lampjavel_users` (`username`),
-    unique (`channel_name`, `username`)
+    `channel_id` varchar(20) not null,
+    `user_id` varchar(20) not null,
+    foreign key (`channel_id`) references `lampjavel_channels` (`id`),
+    foreign key (`user_id`) references `lampjavel_users` (`id`),
+    unique (`channel_id`, `user_id`)
 );
 
 create table `lampjavel_channel_images` (
-    `channel_name` varchar(20) not null,
+    `channel_id` varchar(20) not null,
     `image_id` int(9) not null,
-    foreign key (`channel_name`) references `lampjavel_channels` (`name`),
+    foreign key (`channel_id`) references `lampjavel_channels` (`id`),
     foreign key (`image_id`) references `lampjavel_images` (`id`),
-    unique (`channel_name`, `image_id`)
+    unique (`channel_id`, `image_id`)
 );
 
 create table `lampjavel_channel_members` (
-    `channel_name` varchar(20) not null,
-    `username` varchar(20) not null,
-    foreign key (`channel_name`) references `lampjavel_channels` (`name`),
-    foreign key (`username`) references `lampjavel_users` (`username`),
-    unique (`channel_name`, `username`)
+    `channel_id` varchar(20) not null,
+    `user_id` varchar(20) not null,
+    foreign key (`channel_id`) references `lampjavel_channels` (`id`),
+    foreign key (`user_id`) references `lampjavel_users` (`id`),
+    unique (`channel_id`, `user_id`)
 );
 
 create table `lampjavel_image_tags` (
     `image_id` int(9) not null,
-    `tag_name` varchar(20) not null,
+    `tag_id` varchar(20) not null,
     foreign key (`image_id`) references `lampjavel_images` (`id`),
-    foreign key (`tag_name`) references `lampjavel_tags` (`name`),
-    unique (`image_id`, `tag_name`)
+    foreign key (`tag_id`) references `lampjavel_tags` (`id`),
+    unique (`image_id`, `tag_id`)
 );
 
-insert into `lampjavel_users` (`username`, `email`, `created`, `updated`) values
+insert into `lampjavel_users` (`id`, `email`, `created_at`, `updated_at`) values
 ('admin', 'admin@admin.com', null, null);
 
-insert into `lampjavel_channels` (`name`, `public`, `created`, `updated`) values
+insert into `lampjavel_channels` (`id`, `is_public`, `created_at`, `updated_at`) values
 ('lampjavel', true, null, null);
 
-insert into `lampjavel_channel_admins` (`channel_name`, `username`) values
+insert into `lampjavel_channel_admins` (`channel_id`, `user_id`) values
 ('lampjavel', 'admin');
 
-insert into `lampjavel_images` (`url`, `created`, `updated`) values
+insert into `lampjavel_images` (`url`, `created_at`, `updated_at`) values
 ('http://i.imgur.com/MRxVkZw.png', null, null);
 
-insert into `lampjavel_channel_images` (`channel_name`, `image_id`) values
+insert into `lampjavel_channel_images` (`channel_id`, `image_id`) values
 ('lampjavel', last_insert_id());
