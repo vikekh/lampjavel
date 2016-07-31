f4e02125631a2b2fa86403e6ca0fb8036fb34800
vikekh <vieekk@gmail.com>
2016-07-31 02:26:57 +0200

    Channel view model

--- public/app/viewModels/channels/index.js
+++ public/app/viewModels/channels/index.js
@@ -1,4 +1,12 @@
 define(['durandal/app', 'jquery', 'knockout', 'dataService', 'viewModels/shell'], function (app, $, ko, dataService, shell) {
+    function Channel(data) {
+        this.id = ko.observable(data.id);
+        this.isPublic = ko.observable(data.isPublic === 1);
+        this.url = ko.computed(function () {
+            return '#channels/' + this.id();
+        }, this);
+    }
+
     return {
         activate: function (channelId) {
             var self = this;
@@ -6,7 +14,9 @@ define(['durandal/app', 'jquery', 'knockout', 'dataService', 'viewModels/shell']
             shell.header('Channels');
 
             return dataService.getChannels().done(function (response) {
-                self.channels(response);
+                for (var i = 0; i < response.length; i++) {
+                    self.channels.push(new Channel(response[i]));
+                }
             });
         },
 
